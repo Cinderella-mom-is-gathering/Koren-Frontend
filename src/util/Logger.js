6a2b7961abde4d19a39b2f12728c2e6787de45c8
sbslc2000 <sbslc2000@gmail.com>
2023-07-29 13:30:08 +0900

    수정 : Logger.js 내부 로그 주석처리

--- src/util/Logger.js
+++ src/util/Logger.js
@@ -3,7 +3,7 @@ import log from 'loglevel'
 //debug, info, warn, error
 let level = process.env.REACT_APP_LOGGING_LEVEL;
 level = level === undefined ? "info" : level;
-console.log("logging-level: "+level);
+//console.log("logging-level: "+level);
 log.setLevel(level);
 const logger = log.getLogger('App');
 export default logger;
