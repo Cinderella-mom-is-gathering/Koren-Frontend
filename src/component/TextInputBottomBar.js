e1a24b5b86647f0a6f93ac49766efce1b2940ae2
galaxy821 <hoshino208@naver.com>
2023-07-31 23:12:30 +0900

    change style textInput

--- src/component/TextInputBottomBar.js
+++ src/component/TextInputBottomBar.js
@@ -9,18 +9,44 @@ const TextInputBottomBarWrapper = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  background-color: #fbfbfb;
+  align-items: center;
+  background-color: #ffffff;
+  padding: 0px 20px;
+  border-top: 1px solid #e5e5e5;
 `;
 
 const AddImageButton = styled.div`
   width: 45px;
   height: 45px;
+  border-radius: 50%;
+    background-color: #5686E3;
+  
+`;
+
+const AddPostButton = styled.div`
+    width: 45px;
+    height: 45px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Text = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+  color: #000000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const TextInputBottomBar = () => {
   return (
     <TextInputBottomBarWrapper>
       <AddImageButton></AddImageButton>
+        <AddPostButton>
+            <Text>{"등록"}</Text>
+        </AddPostButton>
     </TextInputBottomBarWrapper>
   );
 };
