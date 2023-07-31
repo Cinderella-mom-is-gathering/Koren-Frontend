a828b3ec31e7bc68b4e2c306440a57bb927cc30c
galaxy821 <hoshino208@naver.com>
2023-07-31 23:20:34 +0900

    add header in AddPostPage

--- src/page/AddPostPage.js
+++ src/page/AddPostPage.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { TextInput } from "../component/TextInput";
 import { TextInputBottomBar } from "../component/TextInputBottomBar";
+import Header from "../component/header/Header";
 
 const AddPostPageWrapper = styled.div`
   width: 100%;
@@ -10,6 +11,7 @@ const AddPostPageWrapper = styled.div`
 const AddPostPage = () => {
   return (
     <AddPostPageWrapper>
+      <Header renderBackArrowButton={true} title="포스트 작성" />
       <TextInput />
       <TextInputBottomBar />
     </AddPostPageWrapper>
