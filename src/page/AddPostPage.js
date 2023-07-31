30210aeab61a1221667a1a02ee22350e658893fc
galaxy821 <hoshino208@naver.com>
2023-08-01 01:55:33 +0900

    add event listen screen height

--- src/page/AddPostPage.js
+++ src/page/AddPostPage.js
@@ -2,18 +2,29 @@ import styled from "styled-components";
 import { TextInput } from "../component/TextInput";
 import { TextInputBottomBar } from "../component/TextInputBottomBar";
 import Header from "../component/header/Header";
+import { useEffect, useState } from "react";
 
 const AddPostPageWrapper = styled.div`
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-direction: column;
 `;
 
 const AddPostPage = () => {
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    if (window) {
+      let prevVisualViewport = window.visualViewport?.height;
+      console.log(prevVisualViewport);
+    }
+  }, []);
   return (
     <AddPostPageWrapper>
       <Header renderBackArrowButton={true} title="포스트 작성" />
       <TextInput />
-      <TextInputBottomBar />
+      <TextInputBottomBar height={height} />
     </AddPostPageWrapper>
   );
 };
