acdb33e1c9e72af188ecb53cbcbb9d60b6e37573
sbslc2000 <sbslc2000@gmail.com>
2023-07-31 16:58:36 +0900

    routing 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 
 import './App.css';
 import PersonalInfo from "./page/PersonalInfo";
+import PostListPage from "./page/PostListPage";
+import PostDetailPage from "./page/PostDetailPage";
+import AddPostPage from "./page/AddPostPage";
+import PostNFTInfoPage from "./page/PostNFTInfoPage";
+import LoginPage from "./page/LoginPage";
+import RequestAdPage from "./page/RequestAdPage";
+
+import {Routes, Route, HashRouter} from "react-router-dom";
 
 function App() {
   return (
-      <PersonalInfo/>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/posts" element={<PostListPage />} />
+          <Route path="/posts/:postId" element={<PostDetailPage />} />
+          <Route path="/posts/add" element={<AddPostPage />} />
+          <Route path="/posts/:postId/nft" element={<PostNFTInfoPage />} />
+          <Route path="/advertisements/add" element={<RequestAdPage />} />
+        </Routes>
+      </HashRouter>
   );
 }
 
