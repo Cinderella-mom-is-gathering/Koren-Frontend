f099b6af1dee2826a48a8a588f45d296b501cb1a
sbslc2000 <sbslc2000@gmail.com>
2023-08-01 13:07:19 +0900

    temp save

--- src/page/ContractApiTestPage.js
+++ src/page/ContractApiTestPage.js
@@ -1,4 +1,17 @@
 
+import * as Api from "/src/apis/ApiInterface";
 const ContractApiTestPage = () => {
 
+
+    Api.greeting().then((result) => {
+        console.log("greeting")
+        console.log(result);
+    });
+
+    Api.greeting()
+    return (
+        <div>
+            ContractApiTest
+        </div>
+    )
 }
