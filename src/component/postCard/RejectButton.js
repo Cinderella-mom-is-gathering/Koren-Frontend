f3b1902ee7902cea0df6ef37f21c9a23614e1456
sumkyun <70997770+sumkyun@users.noreply.github.com>
2023-08-02 01:53:02 +0900

    승인 거절 버튼 부트스트랩

--- src/component/postCard/RejectButton.js
+++ src/component/postCard/RejectButton.js
@@ -1,11 +1,12 @@
 import React from "react";
 import context from "react-bootstrap/esm/AccordionContext";
-
 import styled from "styled-components";
 import { DeleteContext } from "../../page/ApprovalADPage";
 import { useContext } from "react";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
-const Button = styled.button`
+const Button2 = styled.button`
 
   width: 95px;
   height: 40px;
@@ -20,20 +21,45 @@ const Button = styled.button`
   
 `
 
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header >
+        <Modal.Title id="contained-modal-title-vcenter">
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>
+          거절완료
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide} style={{backgroundColor: "#5686E3"}}>닫기</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
 
 const RejectButton=(props)=>{
+  const [modalShow, setModalShow] = React.useState(false);
+  return (<>
+    <Button2 variant="primary" onClick={() => setModalShow(true)}>
+      거절
+    </Button2>
 
-  const deleteContext = useContext(DeleteContext);
-
-
-  const handleClick = () => {
-    deleteContext.deleteHandler(props.postId);
-  }
-
-    return(
-        <Button onClick={handleClick}>거절</Button>
-
-    )
+    <MyVerticallyCenteredModal
+      show={modalShow}
+      onHide={() => setModalShow(false)}
+    />
+  </>
+  )
 }
+
+
 export default RejectButton;
