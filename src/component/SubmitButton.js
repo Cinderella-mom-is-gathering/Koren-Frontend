776664841f8fc4815319152b5fe111001cdcbbf0
galaxy821 <hoshino208@naver.com>
2023-08-01 14:48:29 +0900

    addPostPage에서 컨트렉트 api 연결

--- src/component/SubmitButton.js
+++ src/component/SubmitButton.js
@@ -1,28 +1,26 @@
 import styled from "styled-components";
 
 const Button = styled.button`
-  
   -webkit-appearance: none;
   -moz-appearance: none;
   appearance: none;
   border: none;
-  position:fixed;
-  bottom:0;
-  width:100%;
-  height:70px;
-  background-color : cornflowerblue;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 70px;
+  background-color: cornflowerblue;
   color: white;
   font-size: 25px;
   font-weight: bold;
   letter-spacing: 5px;
-  
-`
-const SubmitButton=({content})=> {
-    return (
-        <form>
-        <Button type='button'> {content} </Button>
-        </form>
-    );
-}
+`;
+const SubmitButton = ({ content }) => {
+  return (
+    <form>
+      <Button type="button"> {content} </Button>
+    </form>
+  );
+};
 
 export default SubmitButton;
