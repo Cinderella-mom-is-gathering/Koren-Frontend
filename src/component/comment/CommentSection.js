2687c2b4951cc1d3820b82104f3d0c9a214f2e42
Seo Beomseok <sbslc2000@gmail.com>
2023-08-01 17:12:04 +0900

    commment 4

--- src/component/comment/CommentSection.js
+++ src/component/comment/CommentSection.js
@@ -2,12 +2,14 @@
 import comment1 from "../../assets/comment1.svg";
 import comment2 from "../../assets/comment2.svg";
 import comment3 from "../../assets/comment3.svg";
+import comment4 from "../../assets/comment4.svg";
 const CommentSection = () => {
     return (
         <div>
             <img src={comment1}/>
             <img src={comment2}/>
             <img src={comment3}/>
+            <img src={comment4}/>
         </div>
     );
 }
